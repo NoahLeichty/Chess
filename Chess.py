cb0a59e4ced61ddd538be29ef7b1c8d89c15b8ce
NoahLeichty <nleichty1@gmail.com>
2025-08-12 18:24:22 -0500

    Made an assets directory

--- Chess.py
+++ Chess.py
@@ -1,9 +1,22 @@
 # will be like chess and eventually have AI and engine
-import tkinter as tk  
+import tkinter as tk
+from PIL import Image, ImageTk
+import os
 
 # opens frame for chess game
 root = tk.Tk()
 root.title("Chess")
 root.geometry("600x600")
+
+# Create a chessboard layout
+# 8x8 grid of squares
+for i in range(8):
+    for j in range(8):
+        if (i + j) % 2 == 0:
+            color = "white"
+        else:
+            color = "dark blue"
+        square = tk.Frame(root, width=75, height=75, bg=color)
+        square.grid(row=i, column=j)
 
 root.mainloop()
